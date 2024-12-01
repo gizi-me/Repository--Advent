795cdbf34ba4f9b724cb76918f288b9e9dbfd744
gizi-me <190292924+gizi-me@users.noreply.github.com>
2024-12-01 22:03:56 +0100

    add day1.py

---
+++ day1.py
@@ -0,0 +1,37 @@
+# day one od AOC 2024
+import numpy as np
+import os
+
+# Import der Daten
+data = np.loadtxt('input_day1.txt')
+
+# part one: total distance _____________________________________________________________________________
+# Listen ordnen
+
+left = np.sort(data[:,0])
+
+right = np.sort(data[:,1])
+
+# distanz berechnen
+
+dist = np.abs(left - right)
+
+tot_distance = np.sum(dist)
+
+print('total distance:', tot_distance)
+
+# part two: similarity score ____________________________________________________________________________
+def compare(list1, list2):
+    lenght = len(list1)
+    score = 0
+    for i in range(0,lenght):
+        counter = 0
+        for j in range(0,lenght):
+            if list1[i] == list2[j]:
+                counter += 1
+        score += counter * list1[i]
+    return score
+
+print(compare(left,right))
+
+        
